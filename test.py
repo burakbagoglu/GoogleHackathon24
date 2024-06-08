e1c9355fadb0cf53d91b8820db8939b74e2ffb82
Sphcnn <batus-2005@hotmail.com>
2024-06-08 15:04:57 +0300

    Update test.py

--- test.py
+++ test.py
@@ -4,10 +4,29 @@ app = CTk()
 
 
 app.geometry("500x400")
+set_appearance_mode("dark")
 
-button = CTkButton(master=app,text="enter",corner_radius=35,fg_color="#528b8b",hover_color="4158D0",border_color="FFCC70")
 
-button.place(relx=0.5, rely=0.5, anchor="center")
+button = CTkButton(master=app,text="Giriş",corner_radius=35,fg_color="#528b8b",hover_color="#4158D0",border_color="#FFCC70", border_width=2,)
+
+button.place(relx=0.03, rely=0.7 )
+
+button1 = CTkButton(master=app,text="Hesap Oluştur",corner_radius=35,fg_color="#528b8b",hover_color="#4158D0",border_color="#FFCC70", border_width=2,)
+
+button1.place(relx=0.7, rely=0.7)
+
+label1= CTkLabel(master=app,text="Kullanıcı Adı:",font=("Arial",17 ))
+label1.place(relx=0.03,rely=0.07)
+
+entry1= CTkEntry(master=app,fg_color=("#ffffff"))
+entry1.place(relx=0.03,rely=0.15)
+
+label2= CTkLabel(master=app,text="Şifre:",font=("Arial",17 ))
+label2.place(relx=0.03,rely=0.23)
+
+
+entry2= CTkEntry(master=app,fg_color=("#ffffff"))
+entry2.place(relx=0.03,rely=0.31)
 
 
 app.mainloop()
